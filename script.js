ee4b8254c45b79943aebf61e7cd19c517b9c4f0b
marslikbola <mengboyevfarrux5@gmail.com>
2025-02-16 20:43:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeUser() {
+    document.querySelector('.name').textContent = 'New User';
+    document.querySelector('.title').textContent = 'New Position';
+    document.querySelector('.info:nth-of-type(1)').textContent = 'Phone: +998 91 234 56 78';
+    document.querySelector('.info:nth-of-type(2)').textContent = 'Manzil: Samarqand shaxri';
+    document.querySelector('.info:nth-of-type(3)').textContent = 'Yosh: 25';
+    document.querySelector('.info:nth-of-type(4)').textContent = 'Email: newuser@example.com';
+}
